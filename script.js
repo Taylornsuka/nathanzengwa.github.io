28b0adcd863c4a6dbf5c166e608d9e1e99cd4c3a
Taylornsuka <79389311+Taylornsuka@users.noreply.github.com>
2021-09-21 10:45:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-// NAVBAR
-let lastScrollTop = 0;
-navbar = document.getElementById('navbar');
-
-window.addEventListener('scroll', function(){
-  const scrollTop = window.pageTOffset || this.document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop) {
-    navbar.style.top="-50px";
-  } else {
-    navbar.style.top="0";
-  }
-  lastScrollTop = scrollTop;
-
-});
-
-// TYPED
-var typed = new Typed('.typed', {
-  strings: ["Bonjour à tous et à toutes, je me présente je m'appelle Taylor N'suka", "Après une carrière m'ayant fait découvrir plusieurs milieux professionnels et exercer plus de 15 métiers différents. J'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 6 mois je me forme à temps plein en autodidacte aux langages Front tels que CSS, Javascript et son Framework React afin devenir le plus rapidement possible développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder."],
-  typeSpeed: 20,
-});
-  
-// COMPTEUR LIVE
-let compteur = 0;
-
-$(window).scroll(function() {
-
-  const top = $('.counter').offset().top - window.innerHeight;
-
-  if (compteur == 0 && $(window).scrollTop() > top) {
-    $('.counter-value').each(function() {
-      let $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-        countNum : countTo
-      },
-      {
-        duration: 10000,
-        easing: 'swing',
-        step: function() {
-          $this.text(Math.floor(this.countNum));
-        },
-        complete: function() {
-          $this.text(this.countNum);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
         }
-      });
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
-    compteur = 1;
-  }
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Peintre", "Designer", "Web-Designer", "Freelance"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Peintre", "Designer", "Web-Designer", "Freelance"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
 
-//AOS
-AOS.init();
+$(".image").click(function(){
+    $(".image").removeClass("active");
+    $(this).addClass("active");
+})
